e539d0966a6a5f7250f8febd1e14df6c543ca9ce
anderson-rodriguesdev <andersonrodrigues.silva96@gmail.com>
2022-10-31 18:18:47 -0300

    installed stitches.js and configured css global variables

---
+++ src/components/button/index.js
@@ -0,0 +1,5 @@
+import { StyleButton } from './style';
+
+export default function Button() {
+  return <StyleButton>Button</StyleButton>;
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
+import Button from '../components/button';
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <h1>Home</h1>
+      <Button />
+    </>
+  );
 }
